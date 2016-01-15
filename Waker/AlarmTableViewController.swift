87318311b13e1afdacd5e3edf819d0f137a53401
Jacky Chiu <jacky.chiu888@gmail.com>
2016-01-14 21:47:46 -0500

    Started project

---
+++ Waker/AlarmTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AlarmTableViewController.swift
+//  Waker
+//
+//  Created by Jacky Chiu on 2016-01-13.
+//  Copyright © 2016 Jacky Chiu. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+            }
+}
---
+++ Waker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waker
+//
+//  Created by Jacky Chiu on 2016-01-12.
+//  Copyright © 2016 Jacky Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
