3a890f2101302523ca761b2b2078013ac5b8800c
Jacky Chiu <jacky.chiu888@gmail.com>
2016-01-17 23:03:46 -0500

    Updated design

--- Waker/AlarmTableViewController.swift
+++ Waker/AlarmTableViewController.swift
@@ -3,13 +3,28 @@
 //  Waker
 //
 //  Created by Jacky Chiu on 2016-01-13.
-//  Copyright © 2016 Jacky Chiu. All rights reserved.
+//  Copyright © 2016 Jacky Chiu.
 //
 
 import UIKit
 
 class AlarmTableViewController: UITableViewController {
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int)->Int{
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("AlarmCell")! as UITableViewCell
+        cell.backgroundColor = UIColor.clearColor()
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
-            }
+        let img = UIImage(named: "Background_big")
+        self.view.backgroundColor = UIColor(patternImage: img!)
+        tableView.rowHeight = 60
+        tableView.separatorColor = UIColor.clearColor()
+    }
 }
